402c4cb987e691e1ef7b4315c328a2c58b3b816a
BaltazarGalvan <b_galvan@yahoo.com>
2023-09-06 15:44:45 -0600

    post test to determinate the user id

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// const hostname = "127.0.0.1";
+const hostname = "127.0.0.1";
 const port = process.env.PORT || 5000;
 
 const dataArray = [];
