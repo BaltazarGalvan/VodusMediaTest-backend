0b37efabc6457d547627b5b18692d555bbe7390d
BaltazarGalvan <b_galvan@yahoo.com>
2023-09-06 14:33:18 -0600

    git init

---
+++ index.js
@@ -0,0 +1,60 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// const hostname = "127.0.0.1";
+const port = process.env.PORT || 5000;
+
+const dataArray = [];
+
+app.get("/", (req, res) => {
+  console.log(req.query.user);
+  const dataFiltered = dataArray.filter(
+    (data) => data.data.userIdentifier === req.query.user
+  );
+  const videoArray = dataFiltered.map((data) => {
+    return {
+      id: data.data.videoId,
+      extensions: {
+        resumeLastUpdate: data.time,
+        resumeTime: data.data.secondsFromStart,
+        progress: data.data.progress,
+        resumeCompleted: false,
+      },
+    };
+  });
+  const dataToReturn = { entry: videoArray };
+  res.send(dataToReturn);
+});
+
+app.post("/", (req, res) => {
+  const data = req.body;
+  dataArray.push(data);
+  console.log(dataArray);
+  res.send("esta es la respuesta del post");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}/`);
+});
+
+// solo con node js
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World\n");
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
