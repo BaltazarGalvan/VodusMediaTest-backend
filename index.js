02e1e0b72cad08d015d2240cb9ad6ff4af0dbd78
BaltazarGalvan <103952690+BaltazarGalvan@users.noreply.github.com>
2024-02-15 10:41:06 -0600

    adding post status 201

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+/* airtable patlgy4Vt9pGjbfQJ.d37527a72d91386f21f786570631e3c5f0981de7d5109ca2c6d50cb914f5385d */
+
+/* lo que llega desde las apps
+{
+    "data": {
+      "progress": 0,
+      "secondsFromStart": " 0",
+      "sessionStartAt": 0,
+      "status": "VIDEO_STARTED",
+      "userIdentifier": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6ImQxOGZkZDc3LTIzMzgtNGEwNi1iMjZhLTk4ZGVjNTNiOTI2NCJ9.eyJhdWQiOiIxNWI2ODE4YS0wYTA2LTRjM2EtOThiYi0xYjNmMzY2ZjllZGIiLCJqdGkiOiJkMThmZGQ3Ny0yMzM4LTRhMDYtYjI2YS05OGRlYzUzYjkyNjQiLCJpYXQiOjE3MDc5Mjg5MjMsIm5iZiI6MTcwNzkyODkyMywiZXhwIjoxNzEwNTI0NTIzLCJzdWIiOiJiX2dhbHZhbkB5YWhvby5jb20iLCJzY29wZXMiOltdLCJtaWQiOjQzOTg2NTIsImFpZCI6MTEzMTM4ODEsIm11aSI6IjE1YjY4MThhLTBhMDYtNGMzYS05OGJiLTFiM2YzNjZmOWVkYiIsImN0eCI6WyJjb25zdW1lciJdLCJ0aWQiOjExMzEzODgxLCJ0dXVpZCI6ImQxOGZkZDc3LTIzMzgtNGEwNi1iMjZhLTk4ZGVjNTNiOTI2NCIsIm9pZCI6MH0.hiZ0HmwgV6NGa-uMIq4RSHX1Ox3QvuYH0NqM3YdXj0c",
+      "videoId": "vbZk3Cv3"
+    },
+    "datacontenttype": "application/json",
+    "id": "01d4540f-895c-4ed5-922c-eb9a174fbcf0",
+    "source": "kanalddrama://com.appkanalddrama/versions/3.0.6",
+    "specversion": "1.0",
+    "subject": "Watched video segment 0 progress: 0 vbZk3Cv3",
+    "time": "2024-02-14T17:42:20Z",
+    "type": "com.applicaster.video.started.v1"
+}
+
+lo que debe entregar en el get
+
+{
+  "entry": [
+    {
+      "id": "296bed86-11dc-43c3-9aaf-bfb25600ad2a", // the unique id of the video
+      "extensions": {
+          "resumeLastUpdate": <TIMESTAMP>
+          "resumeTime": "<Number of seconds from the beginning of the video from which playback should start when playing this item>",
+          "progress": 0.43 // number between 0 - 1 indecating the progress of the fed
+          "resumeCompleted": true // When set to true, the app will use this info to remove the video from the list
+        }
+      }
+    }, {...}, {...}
+  ]
+}
+
+*/
+
 const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -48,7 +88,9 @@ app.post("/", (req, res) => {
     dataArray.push(dataReceived);
 
   console.log(dataArray);
-  res.send(dataArray);
+  res.send('""');
+  res.status(201).end();
+  // res.send(dataArray);
 });
 
 app.listen(port, () => {
