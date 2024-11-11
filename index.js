22ef0e5306c843a8ac4843d4ac09d9398afba63c
BaltazarGalvan <103952690+BaltazarGalvan@users.noreply.github.com>
2024-11-11 13:28:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.post("/", (req, res) => {
   // )
   //   dataArray.push(dataReceived);
 
-  console.log("Post "+req.query.ctx);
+  console.log(req.subject+req.query.ctx);
   res.send({"specversion": "1.0","type": "com.applicaster.event.received.v1","source": "Vodus Server","subject": "Event  Stopped was successfully received","id": "ID_EVENT","time": "Ahora"});
   //res.send(req.params);
   res.status(201).end();
