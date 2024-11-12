df76e9ab760a09b191d65d72cfe56bee4a9fd62a
BaltazarGalvan <103952690+BaltazarGalvan@users.noreply.github.com>
2024-11-12 09:40:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,22 @@
     "type": "com.applicaster.video.started.v1"
 }*/
 
+/* lo que debe entregar
+{
+  "entry": [
+    {
+      "id": "296bed86-11dc-43c3-9aaf-bfb25600ad2a", // the unique id of the video
+      "extensions": {
+          "resumeLastUpdate": <TIMESTAMP>
+          "resumeTime": "<Number of seconds from the beginning of the video from which playback should start when playing this item>",
+          "progress": 0.43 // number between 0 - 1 indecating the progress of the fed
+          "resumeCompleted": true // When set to true, the app will use this info to remove the video from the list
+        }
+      }
+  ]
+}
+*/
+
 const Airtable = require("airtable");
 const dotenv = require("dotenv");
 const express = require("express");
@@ -81,37 +97,37 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  // const dataReceived = req.body;
+    const dataReceived = {
+        id: req.body.data.videoId,
+        extensions: {
+        resumeLastUpdate: req.body.time,
+        resumeTime: req.body.data.secondsFromStart,
+        progress: req.body.data.progress,
+        resumeCompleted: false,
+        },
+    };
 
-  // if (
-  //   dataReceived.data.status === "VIDEO_STOPPED" ||
-  //   dataReceived.data.status === "COMPLETED"
-  // )
-  //   dataArray.push(dataReceived);
-/* lo que llega desde las apps
-{
-    "data": {
-      "progress": 0,
-      "secondsFromStart": " 0",
-      "sessionStartAt": 0,
-      "status": "VIDEO_STARTED",
-      "userIdentifier": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6ImQxOGZkZDc3LTIzMzgtNGEwNi1iMjZhLTk4ZGVjNTNiOTI2NCJ9.eyJhdWQiOiIxNWI2ODE4YS0wYTA2LTRjM2EtOThiYi0xYjNmMzY2ZjllZGIiLCJqdGkiOiJkMThmZGQ3Ny0yMzM4LTRhMDYtYjI2YS05OGRlYzUzYjkyNjQiLCJpYXQiOjE3MDc5Mjg5MjMsIm5iZiI6MTcwNzkyODkyMywiZXhwIjoxNzEwNTI0NTIzLCJzdWIiOiJiX2dhbHZhbkB5YWhvby5jb20iLCJzY29wZXMiOltdLCJtaWQiOjQzOTg2NTIsImFpZCI6MTEzMTM4ODEsIm11aSI6IjE1YjY4MThhLTBhMDYtNGMzYS05OGJiLTFiM2YzNjZmOWVkYiIsImN0eCI6WyJjb25zdW1lciJdLCJ0aWQiOjExMzEzODgxLCJ0dXVpZCI6ImQxOGZkZDc3LTIzMzgtNGEwNi1iMjZhLTk4ZGVjNTNiOTI2NCIsIm9pZCI6MH0.hiZ0HmwgV6NGa-uMIq4RSHX1Ox3QvuYH0NqM3YdXj0c",
-      "videoId": "vbZk3Cv3"
-    },
-    "datacontenttype": "application/json",
-    "id": "01d4540f-895c-4ed5-922c-eb9a174fbcf0",
-    "source": "kanalddrama://com.appkanalddrama/versions/3.0.6",
-    "specversion": "1.0",
-    "subject": "Watched video segment 0 progress: 0 vbZk3Cv3",
-    "time": "2024-02-14T17:42:20Z",
-    "type": "com.applicaster.video.started.v1"
-}*/
+    const dataToReturn = {
+        specversion: "1.0",
+        type: "com.applicaster.event.received.v1",
+        source: "VM Backend Server",
+        subject: "Event " + req.body.data.status + " was successfully received.",
+        id: req.body.id,
+        time: req.body.time
+        }
+    };
+    /*   req.body.data.status === "COMPLETED"*/
+    if (req.body.data.status === "VIDEO_STOPPED" ){ 
+        dataArray.push(dataReceived);
+        console.log("Agregado.");
+    }
 
   //console.log(req.query.ctx+" "+req.body.datacontenttype+" "+req.body.subject+" "+req.body.data.userIdentifier);
-  console.log("VideoID:"+req.body.data.videoId+" status:"+req.body.data.status+" Progress"+req.body.data.progress+" Userid:"+req.body.data.userIdentifier);
-  res.send({"specversion": "1.0","type": "com.applicaster.event.received.v1","source": "Vodus Server","subject": "Event  Stopped was successfully received","id": "ID_EVENT","time": "Ahora"});
-  //res.send(req.params);
-  res.status(201).end();
+    
+  //console.log("VideoID: "+req.body.data.videoId+" status: "+req.body.data.status+" Progress: "+req.body.data.progress+" Userid: "+req.body.data.userIdentifier);
+    console.log(dataReceived);  
+    res.send(dataToReturn);
+    res.status(201).end();
 });
 
 app.listen(port, () => {
