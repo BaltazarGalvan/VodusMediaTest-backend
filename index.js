b1b11ef04e2cf1e9089f5d5e010f799577a3a091
BaltazarGalvan <103952690+BaltazarGalvan@users.noreply.github.com>
2024-11-14 17:04:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ app.get("/all_records", (req, res) => {
 });
 
 app.get("/",(req,res)=>{
-    console.log('GetRoot ', req.query.ctx, "body ", JSON.stringify(req.body));
+    console.log('GetRoot ', req.query.ctx, "body ", JSON.stringify(req));
     const userId = req.query.ctx;
     const dataToReturn = {};
     // const userRecord = usersArray.find((userRecord)=> userRecord.id ===userId)
